95f04e31732d07f654420297e12e2089e52de3d8
Blake Roddy <you@example.com>
2016-09-14 21:20:41 -0500

    added firebase config to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+ var config = {
+    apiKey: "AIzaSyBFAOdM8NhXt2WV1kH1Whkv_k5xa8L3ZD0",
+    authDomain: "train-hw.firebaseapp.com",
+    databaseURL: "https://train-hw.firebaseio.com",
+    storageBucket: "train-hw.appspot.com",
+    messagingSenderId: "129653901205"
+  };
+  
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
